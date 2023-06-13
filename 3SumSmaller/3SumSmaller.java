c443c8221c70bea34cca9c62fdfdd18abf84aa72
Aarohi Chopra <51353233+AarohiChopra@users.noreply.github.com>
2023-06-13 17:01:05 -0400

    Create 3SumSmaller.java

---
+++ 3SumSmaller/3SumSmaller.java
@@ -0,0 +1,30 @@
+public class 3SumSmaller {
+    /**
+     * @param nums:  an array of n integers
+     * @param target: a target
+     * @return: the number of index triplets satisfy the condition nums[i] + nums[j] + nums[k] < target
+     */
+    public int threeSumSmaller(int[] nums, int target) {
+        Arrays.sort(nums);
+        int count = 0;
+        for(int i = 0; i < nums.length - 1; i++)
+        {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while(j < k)
+            {
+                int temp = nums[i] + nums[j] + nums[k];
+                if(temp >= target)
+                {
+                    k--;
+                }
+                else
+                {
+                    count += (k-j);
+                    j++;
+                } 
+            }
+        }
+        return count;
+    }
+}
